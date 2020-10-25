5b631ea8fa99ae8bafac786401cfb058930afbab
Deepak Gupta <deepak@localhost.localdomain>
2020-10-25 14:58:28 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -8,20 +8,31 @@ import './custom.scss';
 // Remove below code if it is problematic during routing
 const changePage = function(e) {
     let href = e.target.getAttribute("href");
-	if(e.target.getAttribute("classic") == "yes" || href.startsWith("#") || !href)
-    	return 
+    if (e.target.getAttribute("classic") == "yes" || href.startsWith("#") || !href)
+        return
     e.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.open('GET', href);
     xhr.onload = function() {
-        if (xhr.status === 200) {
-			var doc = new DOMParser().parseFromString(xhr.response, 'text/html');	
-    		document.querySelector("html").replaceWith(doc.querySelector("html"));
-    		window.history.pushState({}, '', href)
-    		//add listeners again when dom is recreated
-			document.querySelector("a").addEventListener('click', changePage, true);			
-        } else {
-            console.log("Unable to load page")
+        try {
+            if (xhr.status === 200) {
+                if (xhr.getResponseHeader('Content-Type').includes("text/html")) {
+                    var doc = new DOMParser().parseFromString(xhr.response, 'text/html');
+                    document.querySelector("html").replaceWith(doc.querySelector("html"));
+                    window.history.pushState({}, '', href)
+                        //add listeners again when dom is recreated
+                    document.querySelector("a").addEventListener('click', changePage, true);
+                } else {
+                    throw Error("Invalid HTML Page");
+                }
+
+            } else {
+                throw Error("Error Occured. Trying Default Redirect");
+            }
+
+        } catch (e) {
+            console.log(e.message)
+            window.location = href;
         }
     };
     xhr.send();
